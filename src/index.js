ed2511142de30f27a2589a2288b2a22f03fa0481
JasonMihari <75113663+JasonMihari@users.noreply.github.com>
2025-03-06 22:36:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Musico from './src/components/Musico.js'; // Ensure correct path
+import Musico from './components/Musico.js'; // Ensure correct path
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
