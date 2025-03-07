2de017d0764dec0d613ffb47dc8e58efb5d5397a
JasonMihari <75113663+JasonMihari@users.noreply.github.com>
2025-03-06 22:24:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Musico from './components/Musico'; // Import Musico instead of App
+import Musico from './components/Musico.js'; // Ensure correct path
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Musico /> {/* Render Musico instead of App */}
+    <Musico /> {/* Render Musico */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
 
